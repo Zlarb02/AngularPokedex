4c48659545b5a605c90579eca46a71f03615344c
Etienne Pogoda <pogoda.etienne@outlook.fr>
2024-06-02 17:28:57 +0200

    fetch french infos, display details in 2 separate windows for right screen, accessible with buttons 0 to 9

--- src/app/page/create-pokemon/create-pokemon.component.ts
+++ src/app/page/create-pokemon/create-pokemon.component.ts
@@ -6,11 +6,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { ZoomService } from '../../shared/zoom.service';
 import { PokedexService } from '../../shared/pokedex.service';
+import { Observable, forkJoin, map, pluck } from 'rxjs';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-create-pokemon',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, HttpClientModule],
   templateUrl: './create-pokemon.component.html',
   styleUrls: ['./create-pokemon.component.css']
 })
@@ -21,7 +23,8 @@ export class CreatePokemonComponent {
   constructor(
     private fb: FormBuilder,
     private zoomService: ZoomService,
-    private pokedexService: PokedexService
+    private pokedexService: PokedexService,
+    private http: HttpClient  // Ajoutez ceci
 
   ) {
     this.pokemonForm = this.fb.group({
@@ -33,7 +36,7 @@ export class CreatePokemonComponent {
 
   urlValidator(control: FormControl): { [key: string]: boolean } | null {
     const urlPattern = /^(https?:\/\/).*\.(png|jpg|jpeg|gif|bmp)$/i;
-    if (!control.value.match(urlPattern)) {
+    if (control.value && !control.value.match(urlPattern)) {
       return { invalidUrl: true };
     }
     return null;
@@ -44,31 +47,46 @@ export class CreatePokemonComponent {
     this.pokemonForm.reset();
   }
 
+  getRandomPokemonDetail(): Observable<any> {
+    const randomId = Math.floor(Math.random() * 1000);
+    return this.http.get<any>(`https://pokeapi.co/api/v2/pokemon/${randomId}`)
+      .pipe(
+        map((pokemonDetails: Pokemon) => ({
+          height: pokemonDetails.height,
+          weight: pokemonDetails.weight,
+          moves: pokemonDetails.moves.map((move: any) => move.move.name),
+          types: pokemonDetails.types.map((type: any) => type.type.name),
+          abilities: pokemonDetails.abilities.map((ability: any) => ability.ability.name),
+        }))
+      );
+  }
+
   onSubmit(): void {
     if (this.pokemonForm.valid) {
       const { name, image, description } = this.pokemonForm.value;
-      const randomId = Math.floor(Math.random() * 151) + 1;
-      const newPokemon: Pokemon = {
-        id: this.pokedexService.getNextPokemonId(),
-        name: name,
-        sprite: image,
-        back_default: undefined,
-        front_shiny: undefined,
-        back_shiny: undefined,
-        height: 0,
-        weight: 0,
-        moves: [],
-        types: [],
-        abilities: [],
-        description: description,
-        cryUrlLatest: `https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/${randomId}.ogg`
-      };
-
-      this.pokedexService.addPokemon(newPokemon);
-      console.log('Form submitted', newPokemon);
-      this.isFormSubmitted = true;
-    } else {
-      console.log('Form is invalid');
+      const randomId = Math.floor(Math.random() * 1000);
+      forkJoin({
+        details: this.getRandomPokemonDetail()
+      }).subscribe(({ details }) => {
+        const newPokemon: Pokemon = {
+          id: this.pokedexService.getNextPokemonId(),
+          name: name,
+          sprite: image,
+          back_default: undefined,
+          front_shiny: undefined,
+          back_shiny: undefined,
+          height: details.height,
+          weight: details.weight,
+          moves: details.moves,
+          types: details.types,
+          abilities: details.abilities,
+          description: description,
+          cryUrlLatest: `https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/${randomId}.ogg`
+        };
+        this.pokedexService.addPokemon(newPokemon);
+      });
     }
   }
+
+
 }
--- src/app/shared/pokedex.service.ts
+++ src/app/shared/pokedex.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, forkJoin } from 'rxjs';
-import { map, mergeMap } from 'rxjs/operators';
+import { concatMap, map, mergeMap } from 'rxjs/operators';
 import { Pokemon } from './pokemon.model';
 
 @Injectable({
@@ -26,36 +26,59 @@ export class PokedexService {
   getNextPokemonId(): number {
     return this.pokemonsSubject.value.length + 1;
   }
-
   loadPokemons(): void {
     this.http.get<any>('https://pokeapi.co/api/v2/pokemon?limit=1000')
       .pipe(
         map(response => response.results),
-        mergeMap((pokemonEntries: any[]) => {
+        concatMap((pokemonEntries: any[]) => { // Remplacer mergeMap par concatMap
           const pokemonDetailsRequests = pokemonEntries.map(pokemon => {
             const id = this.extractIdFromUrl(pokemon.url);
             return this.http.get<any>(`https://pokeapi.co/api/v2/pokemon-species/${id}/`)
               .pipe(
-                mergeMap(speciesData => {
+                concatMap(speciesData => { // Remplacer mergeMap par concatMap
                   const frenchNameEntry = speciesData.names.find((name: any) => name.language.name === 'fr');
                   const flavorTextEntry = speciesData.flavor_text_entries.find((entry: any) => entry.language.name === 'fr');
                   const cryUrlLatest = `https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/${id}.ogg`;
                   return this.http.get<any>(pokemon.url).pipe(
-                    map(pokemonDetails => ({
-                      id: pokemonDetails.id,
-                      name: frenchNameEntry ? frenchNameEntry.name : pokemonDetails.name,
-                      sprite: pokemonDetails.sprites?.front_default,
-                      back_default: pokemonDetails.sprites?.back_default,
-                      front_shiny: pokemonDetails.sprites?.front_shiny,
-                      back_shiny: pokemonDetails.sprites?.back_shiny,
-                      height: pokemonDetails.height,
-                      weight: pokemonDetails.weight,
-                      moves: pokemonDetails.moves.map((move: any) => move.move.name),
-                      types: pokemonDetails.types.map((type: any) => type.type.name),
-                      abilities: pokemonDetails.abilities.map((ability: any) => ability.ability.name),
-                      description: flavorTextEntry ? flavorTextEntry.flavor_text : '',
-                      cryUrlLatest: cryUrlLatest
-                    }))
+                    concatMap(pokemonDetails => { // Remplacer mergeMap par concatMap
+                      const abilityRequests = pokemonDetails.abilities.map((ability: any) => {
+                        return this.http.get<any>(`https://pokeapi.co/api/v2/ability/${ability.ability.name}/`).pipe(
+                          map(abilityData => {
+                            const frenchAbilityNameEntry = abilityData.names.find((name: any) => name.language.name === 'fr');
+                            return frenchAbilityNameEntry ? frenchAbilityNameEntry.name : ability.ability.name;
+                          })
+                        );
+                      });
+                      const typeRequests = pokemonDetails.types.map((type: any) => {
+                        return this.http.get<any>(`https://pokeapi.co/api/v2/type/${type.type.name}/`).pipe(
+                          map(typeData => {
+                            const frenchTypeNameEntry = typeData.names.find((name: any) => name.language.name === 'fr');
+                            return frenchTypeNameEntry ? frenchTypeNameEntry.name : type.type.name;
+                          })
+                        );
+                      });
+                      return forkJoin([...abilityRequests, ...typeRequests]).pipe(
+                        map(results => {
+                          const abilities = results.slice(0, abilityRequests.length);
+                          const types = results.slice(abilityRequests.length, abilityRequests.length + typeRequests.length);
+                          const moves = results.slice(abilityRequests.length + typeRequests.length);
+                          return {
+                            id: pokemonDetails.id,
+                            name: frenchNameEntry ? frenchNameEntry.name : pokemonDetails.name,
+                            sprite: pokemonDetails.sprites?.front_default,
+                            back_default: pokemonDetails.sprites?.back_default,
+                            front_shiny: pokemonDetails.sprites?.front_shiny,
+                            back_shiny: pokemonDetails.sprites?.back_shiny,
+                            height: parseFloat((pokemonDetails.height * 0.1).toFixed(2)),
+                            weight: parseFloat((pokemonDetails.weight * 0.1).toFixed(2)),
+                            types: types,
+                            abilities: abilities,
+                            description: flavorTextEntry ? flavorTextEntry.flavor_text : "Pokemon mystérieux.",
+                            cryUrlLatest: cryUrlLatest
+                          };
+                        })
+                      );
+                    })
                   );
                 })
               );
@@ -69,6 +92,7 @@ export class PokedexService {
         this.loadingSubject.next(false);  // Mettre à jour l'état du chargement à false
       });
   }
+
 
   private extractIdFromUrl(url: string): number {
     const parts = url.split('/');
